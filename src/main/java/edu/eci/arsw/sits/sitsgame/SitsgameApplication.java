fbef9243ddae58b5421e49fdb139c7736b02f99b
andres Rodriguez <rodriguezandres160918@gmail.com>
2025-03-25 14:59:57 -0500

    first commit

---
+++ src/main/java/edu/eci/arsw/sits/sitsgame/SitsgameApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.sits.sitsgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitsgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitsgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/sits/sitsgame/SitsgameApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.sits.sitsgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitsgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
